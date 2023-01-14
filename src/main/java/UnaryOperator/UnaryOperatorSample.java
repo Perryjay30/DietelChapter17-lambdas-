5be07165acd323fdd9ee736ac51a8d79a0c1f820
Perryjay30 <pelumijsh@gmail.com>
2023-01-13 14:30:41 -1200

    Unary Operator Functional Interface

---
+++ src/main/java/UnaryOperator/UnaryOperatorSample.java
@@ -0,0 +1,11 @@
+package UnaryOperator;
+
+import java.util.function.UnaryOperator;
+
+public class UnaryOperatorSample {
+    public static void main(String[] args) {
+        UnaryOperator<Integer> unaryOperator = (number) -> number * number;
+        Integer result = unaryOperator.apply(10);
+        System.out.println(result);
+    }
+}
