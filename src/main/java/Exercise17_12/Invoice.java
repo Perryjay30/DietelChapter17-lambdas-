bc8e259ddd009977aef54775cdce20b65ff2e5de
Perryjay30 <pelumijsh@gmail.com>
2023-01-15 02:22:03 -1200

    Exercise17_12

---
+++ src/main/java/Exercise17_12/Invoice.java
@@ -0,0 +1,69 @@
+package Exercise17_12;
+
+public class Invoice {
+    private String partNumber;
+    private String partDescription;
+    private int quantity;
+    private double pricePerItem;
+
+    public Invoice(String partNumber, String partDescription, int quantity, double pricePerItem) {
+        this.partNumber = partNumber;
+        this.partDescription = partDescription;
+        this.quantity = quantity;
+        this.pricePerItem = pricePerItem;
+    }
+
+    public String getPartNumber() {
+        return partNumber;
+    }
+
+    public void setPartNumber(String partNumber) {
+        this.partNumber = partNumber;
+    }
+
+    public String getPartDescription() {
+        return partDescription;
+    }
+
+    public void setPartDescription(String partDescription) {
+        this.partDescription = partDescription;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPricePerItem() {
+        return pricePerItem;
+    }
+
+    public void setPricePerItem(double pricePerItem) {
+        this.pricePerItem = pricePerItem;
+    }
+
+    public String getQuanDes() {
+        return String.format("%s  %s", getPartDescription(), getQuantity());
+    }
+
+    public double priceAndQuantity() {
+        return getQuantity() * getPricePerItem();
+    }
+
+    public String getInvoiceValue() {
+        return String.format("%s  %s", getPartDescription(), priceAndQuantity());
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "partNumber='" + partNumber + '\'' +
+                ", partDescription='" + partDescription + '\'' +
+                ", quantity=" + quantity +
+                ", pricePerItem=" + pricePerItem +
+                '}';
+    }
+}
